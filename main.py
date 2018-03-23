30d747fd8b3bcf446b973877d3815687f19abca4
Jon Eric Esobedo <joneric@Jons-MacBook-Air.local>
2018-03-22 22:17:06 -0700

    refactor: organize variable names and create functions

--- main.py
+++ main.py
@@ -19,38 +19,8 @@ app = Flask(__name__)
 cors = CORS(app, resources={r'/api/*': {'origins': '*'}})
 app.jinja_loader = jinja2.FileSystemLoader('app/dist')
 
-# API Route `/api/data/stock` - Fetches stock quote and logo
-@app.route('/api/data/stock')
-def fetch_quote():
-    ticker = request.args.get('ticker')
-    custom_quote_url = 'https://api.iextrading.com/1.0/stock/{STOCK}/batch?types=quote,chart&range=1m'.format(STOCK=ticker)
-    custom_logo_url = 'https://api.iextrading.com/1.0/stock/{STOCK}/logo'.format(STOCK=ticker)
-    raw_stock_quote_response = requests.get(custom_quote_url).json()
-    raw_stock_logo_response = requests.get(custom_logo_url).json()
-    stock_quote_high = raw_stock_quote_response['quote']['high']
-    stock_quote_low = raw_stock_quote_response['quote']['low']
-    stock_quote_avg = str(round((stock_quote_high + stock_quote_low) / 2, 2))
-    stock_quote_yesterday = (raw_stock_quote_response['chart'])[-1]
-    stock_quote_yesterday_high = stock_quote_yesterday['high']
-    stock_quote_yesterday_low = stock_quote_yesterday['low']
-    stock_quote_yesterday_avg = str(round((stock_quote_yesterday_high + stock_quote_yesterday_low) / 2, 2))
-    stock_predict = predictStock(int(float(stock_quote_avg)), int(float(stock_quote_yesterday_avg)))
-
-    response = {
-        'stock': {
-            'logo': raw_stock_logo_response['url'],
-            'ticker': raw_stock_quote_response['quote']['symbol'],
-            'company': raw_stock_quote_response['quote']['companyName'],
-            'quote': stock_quote_avg
-        },
-        'prediction': {
-            'tomorrow': stock_predict
-        }
-    }
-
-    return jsonify(response)
-
-def predictStock(p, p_1):
+# Utility functions
+def predict_stock_algorithm_1(p, p_1):
     # Step 1
     # delta = (p - p-1) / (t - t-1) where (t-t-1) will equal 1
     delta = (p - p_1) / 1
@@ -60,12 +30,55 @@ def predictStock(p, p_1):
     p_predict = ((delta)*1) + p
     return p_predict
 
+def calculate_average(high, low):
+    return round(((high + low) / 2), 2)
+
+# API Route `/api/data/stock` - Fetches stock quote and logo
+@app.route('/api/data/stock')
+def fetch_quote():
+    # Get ticker symbol from FE
+    ticker = request.args.get('ticker')
+    quote_url = 'https://api.iextrading.com/1.0/stock/{STOCK}/batch?types=quote,chart&range=1m'.format(STOCK=ticker)
+    logo_url = 'https://api.iextrading.com/1.0/stock/{STOCK}/logo'.format(STOCK=ticker)
+
+    # Fetch stock quote
+    raw_quote_response = requests.get(quote_url).json()
+    raw_logo_response = requests.get(logo_url).json()
+
+    # Calculate today's stock quote average
+    quote_high = raw_quote_response['quote']['high']
+    quote_low = raw_quote_response['quote']['low']
+    quote_avg = str(calculate_average(quote_high, quote_low))
+
+    # Calculate yesterday's stock quote average
+    quote_yesterday_high = (raw_quote_response['chart'])[-1]['high']
+    quote_yesterday_low = (raw_quote_response['chart'])[-1]['low']
+    quote_yesterday_avg = str(calculate_average(quote_yesterday_high, quote_yesterday_low))
+
+    # Prediction algorithm #1
+    quote_prediction = predict_stock_algorithm_1(int(float(quote_avg)), int(float(quote_yesterday_avg)))
+
+    response = {
+        'stock': {
+            'logo': raw_logo_response['url'],
+            'ticker': raw_quote_response['quote']['symbol'],
+            'company': raw_quote_response['quote']['companyName'],
+            'quote': quote_avg
+        },
+        'prediction': {
+            'tomorrow': quote_prediction
+        }
+    }
+
+    return jsonify(response)
+
+# API Route `/api/data/scientist` - Returns raw TSLA stock response for the data scientist
 @app.route('/api/data/scientist')
 def fetch_quote_scientist():
     custom_quote_url = 'https://api.iextrading.com/1.0/stock/tsla/batch?types=quote,chart&range=1m'
-    raw_stock_quote_response = requests.get(custom_quote_url).json()
+    raw_quote_response = requests.get(custom_quote_url).json()
 
-    return jsonify(raw_stock_quote_response)
+    return jsonify(raw_quote_response)
 
 # Serves up Vue frontend on application load
 @app.route('/', defaults={'path': ''})
